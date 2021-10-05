1ffb1df7cdf7d339a07fc2679613fcb20808212f
markmburns <88446677+markmburns@users.noreply.github.com>
2021-10-05 16:06:28 +0100

    Delete PowerShell/Basic-Install directory

--- PowerShell/Basic-Install/setup.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-REM run the powershell
-REM take input to choose version, otherwise default to latest
-powershell -ex bypass -file .\Basic-Install.ps1 %1
-ECHO Restart after pause
-pause
-wpeutil reboot
